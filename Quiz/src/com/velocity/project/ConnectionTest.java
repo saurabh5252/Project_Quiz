b675ad83bbd54f6f57ade1f99e5c048f5c3fbe8b
saurabh5252 <saurabhsc2411@gmail.com>
2022-09-20 17:11:06 +0530

    Added Files

--- Quiz/src/com/velocity/project/ConnectionTest.java
+++ Quiz/src/com/velocity/project/ConnectionTest.java
@@ -1,5 +1,19 @@
 package com.velocity.project;
 
-public class ConnectionTest {
+import java.sql.DriverManager;
+import java.sql.Connection;
 
+public class ConnectionTest {
+	
+	Connection connection = null;
+	public Connection getConnectionDetails() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/quiz","root","Saurabh@2411");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return connection;
+
+}
 }
--- Quiz/src/com/velocity/project/UserInput.java
+++ Quiz/src/com/velocity/project/UserInput.java
@@ -1,6 +1,31 @@
 package com.velocity.project;
 
-public class UserInput {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class UserInput  {
+	
+		PreparedStatement prs = null;
+		Connection con = null;
+
+		public  void insertParticipants(String StudentName, int Score, String Class) {
+			try {
+				ConnectionTest connectiontest = new ConnectionTest();
+				con = connectiontest.getConnectionDetails();
+				prs = con.prepareStatement("insert into data(StudentName,Score,Class)values(?,?,?)");
+				prs.setString(1, Performance.getStudentName());
+				prs.setInt(2, Performance.getStudentScore() );
+				prs.setString(3, Performance.getStudentClass());
+				int i = prs.executeUpdate();
+				
+				
+				System.out.println("Record is Update Successfully" + i);
+				
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 
 	
 	
