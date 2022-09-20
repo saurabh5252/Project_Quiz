dd07a14be4941250b45d3658860a3e902296e91b
vishal23github <vishalnimkarde23@gmail.com>
2022-09-20 17:58:33 +0530

    add performance

--- Quiz/src/com/velocity/project/Performance.java
+++ Quiz/src/com/velocity/project/Performance.java
@@ -1,5 +1,26 @@
 package com.velocity.project;
 
 public class Performance {
+public static String  StudentName;
+public static int StudentScore;
+public static int StudentClass;
+public static String getStudentName() {
+	return StudentName;
+}
+public static void setStudentName(String studentName) {
+	StudentName = studentName;
+}
+public static int getStudentScore() {
+	return StudentScore;
+}
+public static void setStudentScore(int studentScore) {
+	StudentScore = studentScore;
+}
+public static int getStudentClass() {
+	return StudentClass;
+}
+public static void setStudentClass(int studentClass) {
+	StudentClass = studentClass;
+}
 
 }
